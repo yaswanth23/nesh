3578aa7b7e7414e12fa9681ccfd9302b678d6eb5
yash <yashwanth.k@mooo.farm>
2023-02-03 09:57:24 +0530

    logo text changes

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,13 +1,12 @@
 import { Fragment, memo } from "react";
 import { Outlet } from "react-router-dom";
+import "./navigation.styles.css";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div>
-        <h1>
-          Nesh
-        </h1>
+        <h1 className="logo">Nesh</h1>
       </div>
       <Outlet />
     </Fragment>
