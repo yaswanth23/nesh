48c36257563f75fd480c572aa5e8ecef0fcdc6dc
yash <yashwanth.k@mooo.farm>
2023-02-05 14:32:52 +0530

    on hover play sound

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,15 +1,24 @@
-import { Fragment, memo } from "react";
+import { Fragment, memo, useRef } from "react";
 import { Outlet, Link } from "react-router-dom";
+import BtnAudio from "../../assets/audio/btn_click.mp3";
 import "./navigation.styles.css";
 
 const Navigation = () => {
+  const audioRef = useRef();
+
+  const handleMouseEnter = () => {
+    audioRef.current.play();
+  };
   return (
     <Fragment>
       <div className="bg">
         <div className="logo-container">
-          <Link to="/" className="logo">
-            Nesh
-          </Link>
+          <span onMouseEnter={handleMouseEnter}>
+            <Link to="/" className="logo">
+              Nesh
+            </Link>
+            <audio preload="auto" ref={audioRef} src={BtnAudio}></audio>
+          </span>
         </div>
         <Outlet />
       </div>
